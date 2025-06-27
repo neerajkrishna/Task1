198362fe6189ac15c0ee3c2e9461469206b81774
Neeraj Krishna <krishnaneeraj1998@gmail.com>
2025-06-28 01:27:32 +0530

    feat: add middleware

---
+++ src/app/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const WHITELISTED_IPS = ["20.218.226.24"];
+// "152.58.200.125"
+
+export function middleware(req: NextRequest) {
+  const forwarded = req.headers.get("x-forwarded-for");
+  const ip = forwarded?.split(",")[0]?.trim() || "";
+  if (!WHITELISTED_IPS.includes(ip)) {
+    return new NextResponse(`Access denied. ip:${ip}`, {
+      status: 403,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/",
+};
