1658fe315f901c88dd2b2c7f01e195f43b7476d4
neerajkrishna <krishnaneeraj1998@gmail.com>
2025-06-29 18:28:50 +0200

    updating readme file and also removing the unwanted ip

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
-//const WHITELISTED_IPS = "20.218.226.24";
-const WHITELISTED_IPS = "37.201.117.4";
+const WHITELISTED_IPS = "20.218.226.24";
 export function middleware(req: NextRequest) {
   const forwarded = req.headers.get("x-forwarded-for");
   const ip = forwarded?.split(",")[0]?.trim() || "";
